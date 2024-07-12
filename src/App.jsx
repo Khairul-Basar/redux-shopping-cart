6ac1261c00267dcbaec769f51e5354bfe5541800
Khairul-Basar <khairulbasar130@gmail.com>
2024-07-13 01:49:40 +0600

    The redux shopping cart connect with firebase-realtime-database

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,8 +7,21 @@ import Layout from "./components/Layout";
 import { useSelector } from "react-redux";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch(
+        "https://redux-shopping-cart-f50a1-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+    };
+    sendRequest();
+  }, [cart]);
   return (
     <div>
       {!isLoggedIn && <Auth />}
