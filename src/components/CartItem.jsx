0127e9d1c91d239fbdcc126899218dc7ea02a67f
Khairul-Basar <khairulbasar130@gmail.com>
2024-07-13 00:47:32 +0600

    Handle Increament on cart product quantity

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./Cart.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../features/cart/cartSlice";
 const CartItem = ({ name, quantity, total, price, id }) => {
-  const handleIncrement = () => {};
+  const dispatch = useDispatch();
+  const handleIncrement = () => {
+    dispatch(
+      cartActions.addToCart({
+        id,
+        name,
+        price,
+      })
+    );
+  };
   return (
     <div className="cartItem">
       <h2> {name}</h2>
@@ -9,7 +20,9 @@ const CartItem = ({ name, quantity, total, price, id }) => {
       <p>x{quantity}</p>
       <article>Total ${total}</article>
       <button className="cart-actions">-</button>
-      <button className="cart-actions">+</button>
+      <button className="cart-actions" onClick={handleIncrement}>
+        +
+      </button>
     </div>
   );
 };
